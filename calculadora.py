d8f4f8e8b766cbd14670ee4e4d7ee6c75c599d11
ruela <luis.ruelas71@gmail.com>
2026-09-12 21:26:36 -0700

    Areglo

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,10 @@
 class calculadora:
     def add(self, a, b):
         return a + b
+
+    def add_v2(self, a, b):
+            return a * b
+
+    def add_v3(self, a, b):
+                return a ** b
+    
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,3 +1,13 @@
+from calculadora import calculadora
+
 def test_add():
     calc = calculadora()
-    assert calc.add(2, 3) == 6
+    assert calc.add(2, 3) == 5
+
+def test_add_v2():
+    calc = calculadora()
+    assert calc.add_v2(2, 3) == 6
+
+def test_add_v3():
+    calc = calculadora()
+    assert calc.add_v3(2, 3) == 8
